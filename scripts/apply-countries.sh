f2dafeb8856c89b9294347c8e32539929d523d71
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-12-25 13:15:29 -0500

    update-countries: don't update films, just create new entries

--- scripts/apply-countries.sh
+++ scripts/apply-countries.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+set -xe
+
 tdir=$(mktemp -d)
 echo $tdir
 for country in $(fetch-countries); do
   echo "Applying country: $country"
   fetch-country-list -N -c $country -o $tdir/list.csv
-  fetch-directors -N -i $tdir/list.csv -o $tdir/directors.csv
+  fetch-directors -i $tdir/list.csv -o $tdir/directors.csv
   populate-directors -d $tdir/directors.csv
-  make run-all  # fetch all films after each list so that "film already known" heuristic considers just added directors/movies
+  make run-update-directors # fetch all films after each list so that "film already known" heuristic considers just added directors/movies
+  rm $tdir/*.csv
 done
 echo "Done applying countries; results are in $tdir"
 # rm -rf $tdir

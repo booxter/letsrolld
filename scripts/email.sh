7adf57d5c7187841170b48ec733b8654a777f08f
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-12-28 13:34:06 -0500

    Add support for ssmtp for email script

--- scripts/email.sh
+++ scripts/email.sh
@@ -1,16 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -e
 
 FROMADDR=ihar.hrachyshka@gmail.com
-TOADDRS=$FROMADDR
+
+# Format: e@mail1[,e@mail2,...]
+TOADDRS=${TOADDRS:-$FROMADDR}
 
 tmpdir=$(mktemp -d)
+trap 'rm -r $tmpdir' EXIT
+
 reportfile=$tmpdir/movies.report
 ./scripts/gen_report.sh -s "$reportfile"
 
-mailsend-go -from $FROMADDR -t $TOADDRS -sub "Movies for $(date '+%Y-%m-%d')" \
-  -use gmail auth -user $FROMADDR -pass "$(pass priv/google.com-mutt)" \
-  body -file "$reportfile"
+subject="Movies for $(date '+%Y-%m-%d')"
 
-rm -r "$tmpdir"
+if command -v mailsend-go > /dev/null; then
+  mailsend-go -from $FROMADDR -t "$TOADDRS" -sub "$subject" \
+    -use gmail auth -user $FROMADDR -pass "$(pass priv/google.com-mutt)" \
+    body -file "$reportfile"
+else
+  if command -v ssmtp > /dev/null; then
+    echo "Subject: $subject" > "$tmpdir/email.txt"
+    cat "$reportfile" >> "$tmpdir/email.txt"
+
+    # Convert TOADDRS into array.
+    IFS=',' read -r -a TOADDRS <<< "$TOADDRS"
+
+    ssmtp -d99 "${TOADDRS[@]}" < "$tmpdir/email.txt"
+  else
+    echo "No email client found. Please install mailsend-go or ssmtp."
+    exit 1
+  fi
+fi

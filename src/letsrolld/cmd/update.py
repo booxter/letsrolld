5e734ed93399d87a915dd543946af67448d9bd52
Ihar Hrachyshka <ihar.hrachyshka@gmail.com>
2024-12-27 20:45:52 -0500

    Speed up update by fetching multiple objects at once (100)

--- src/letsrolld/cmd/update.py
+++ src/letsrolld/cmd/update.py
@@ -58,7 +58,7 @@ _MODEL_TO_ORDER_BY = {
 }
 
 
-def get_obj_to_update(session, model, threshold, last_checked_field, seen, match):
+def get_objs_to_update(session, model, threshold, last_checked_field, seen, match):
     return (
         session.execute(
             select(model)
@@ -69,10 +69,10 @@ def get_obj_to_update(session, model, threshold, last_checked_field, seen, match
             .order_by(
                 nullsfirst(last_checked_field), *(_MODEL_TO_ORDER_BY.get(model, []))
             )
-            .limit(1)
+            .limit(100)
         )
         .scalars()
-        .first()
+        .all()
     )
 
 
@@ -357,25 +357,26 @@ def run_update(
         i += 1
 
     while True:
-        obj = get_obj_to_update(
+        objs = get_objs_to_update(
             session, model, threshold, last_checked_field, seen, match
         )
-        if obj is None:
+        if not objs:
             break
 
-        if skip_obj(obj, last_updated_field, threshold_func, threshold):
-            loop_housekeeping(session, obj, updated=False)
-            continue
+        for obj in objs:
+            if skip_obj(obj, last_updated_field, threshold_func, threshold):
+                loop_housekeeping(session, obj, updated=False)
+                continue
 
-        print(
-            f"{i}/{n_objs}: Updating {model_name}: {obj.name} @ {obj.lb_url}",
-            flush=True,
-        )
+            print(
+                f"{i}/{n_objs}: Updating {model_name}: {obj.name} @ {obj.lb_url}",
+                flush=True,
+            )
 
-        api_obj = api_cls(obj.lb_url)
-        refresh_func(session, obj, api_obj)
+            api_obj = api_cls(obj.lb_url)
+            refresh_func(session, obj, api_obj)
 
-        loop_housekeeping(session, obj, updated=True)
+            loop_housekeeping(session, obj, updated=True)
 
     # TODO: is there a better way to extract plural names?
     print(f"No more {model_name}s to update")
